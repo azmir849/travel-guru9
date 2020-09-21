8dfb80c169cc6ae28608c345faa46d695bc453ae
azmir849 <azmir15-849@diu.edu.bd>
2020-09-21 17:08:35 +0600

    navbar completed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,12 +2,43 @@ import React from 'react';
 import backgroundImg from '../../Image/Rectangle 1.png'
 import travelGuruLogo from '../../Icon/Logo.png'
 import './Home.css'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-           <img className="BackgroundImg" src={backgroundImg} alt=""/>
-           <img className="TravelGuruLogo" src={travelGuruLogo} alt=""/>
+            <div>
+                 <img className="BackgroundImg" src={backgroundImg} alt=""/>
+            </div>
+            <div className="bg-transparent">
+                <img className="TravelGuruLogo" src={travelGuruLogo} alt=""/>
+                <nav className="navbar navbar-light bg-light navForm  bg-transparent">
+                    <form className="form-inline">
+                        <input className="form-control bg-transparent form-text  mr-sm-2" type="search" placeholder="Search your Destination" aria-label="Search"/>
+                    </form>
+                </nav>
+            </div>
+            <div>
+            <nav className="nav navMenuArea">
+                <ul>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/destinations">Destination</Link>
+                    </li>
+                    <li>
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link className="LogIn-btn btn-warning" to="/login">LogIn</Link>
+                    </li>
+                </ul>
+            </nav>
+            </div>
         </div>
     );
 };
