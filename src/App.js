1d5b3d87d755addf3cb6f4288dd9580e39b363f5
azmir849 <azmir15-849@diu.edu.bd>
2020-09-22 00:05:21 +0600

    Components Routing finished

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import {
 } from "react-router-dom";
 import Home from './Components/Home/Home';
 import NoMatch from './Components/NoMatch/NoMatch';
+import News from './Components/News/News';
+import Destination from './Components/Destination/Destination';
+import Blog from './Components/Blog/Blog';
+import Contact from './Components/Contact/Contact';
+
+
 
 function App() {
   return (
@@ -18,6 +24,18 @@ function App() {
         </Route>
           <Route path="/home">
             <Home></Home>
+          </Route>
+          <Route path="/news">
+            <News></News>
+          </Route>
+          <Route path="/destinations">
+           <Destination></Destination>
+          </Route>
+          <Route path="/blog">
+            <Blog></Blog>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
           </Route>
             <Route path="*">
               <NoMatch></NoMatch>
